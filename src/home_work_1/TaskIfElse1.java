eaa8d48fdeec47eaa3a6d78fccae2bfe32f011f1
Alex Mazur <mazur.alieksiei@mail.ru>
2023-04-17 22:46:47 +0300

    Первая версия задания 6.1

--- src/home_work_1/TaskIfElse1.java
+++ src/home_work_1/TaskIfElse1.java
@@ -1,11 +1,19 @@
 package home_work_1;
+
 import java.util.Scanner;
+
 public class TaskIfElse1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input name: ");
         String name = in.nextLine();
-
-
+        if (name.equals("Вася")) {
+            System.out.print("Привет! \nЯ тебя так долго ждал");
+        }
+        if (name.equals("Анастасия")) {
+            System.out.print("Я тебя так долго ждал");
+        } else {
+            System.out.print("Добрый день, а вы кто?");
+        }
     }
 }

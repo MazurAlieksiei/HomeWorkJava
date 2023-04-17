7e645760508499596d21d770d170e444468b25c6
Alex Mazur <mazur.alieksiei@mail.ru>
2023-04-17 21:56:10 +0300

    Сделали возможность и запрос на ввод имени от пользователя

---
+++ src/home_work_1/TaskIfElse1.java
@@ -0,0 +1,11 @@
+package home_work_1;
+import java.util.Scanner;
+public class TaskIfElse1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+
+
+    }
+}

a262b16a3f9cfb79f03e5789bcf18269fdb5f8eb
Alex Mazur <mazur.alieksiei@mail.ru>
2023-05-06 10:53:47 +0300

    Задание 2.3 - update

--- src/home_work_2/arrays/ArraysIterationWithInterface.java
+++ src/home_work_2/arrays/ArraysIterationWithInterface.java
@@ -7,35 +7,19 @@ public class ArraysIterationWithInterface {
         ForOperation forOperation = new ForOperation();
         ForEachOperation forEachOperation = new ForEachOperation();
 
-        doWhileOperation.elementsOutput();
-        System.out.println();
-        doWhileOperation.everySecondElement();
-        System.out.println();
-        doWhileOperation.reversElementsOutput();
-        System.out.println();
+        executeOperations(doWhileOperation);
+        executeOperations(whileOperation);
+        executeOperations(forOperation);
+        executeOperations(forEachOperation);
+    }
 
-        whileOperation.elementsOutput();
+    public static  void executeOperations (IArraysOperation arraysOperation){
+        arraysOperation.elementsOutput();
         System.out.println();
-        whileOperation.everySecondElement();
+        arraysOperation.everySecondElement();
         System.out.println();
-        whileOperation.reversElementsOutput();
+        arraysOperation.reversElementsOutput();
         System.out.println();
-
-        forOperation.elementsOutput();
-        System.out.println();
-        forOperation.everySecondElement();
-        System.out.println();
-        forOperation.reversElementsOutput();
-        System.out.println();
-
-        forEachOperation.elementsOutput();
-        System.out.println();
-        forEachOperation.everySecondElement();
-        System.out.println();
-        forEachOperation.reversElementsOutput();
-        System.out.println();
-
-
-
     }
 }
+
